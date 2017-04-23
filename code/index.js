e4d358a08720be37c299dc8002fdb8b933111b3e
hsiao-chi Wu <ciyalnin@gmail.com>
2017-04-23 22:53:40 +0800

    Add files via upload
    first

---
+++ code/index.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function() {      
+     $('.dc-menu-trigger').click(function(){
+        $('nav').toggleClass( "dc-menu-open" );
+        $('.menu-overlay').toggleClass( "open" );
+        $('header').toggleClass( "shownav" );
+     }); 
+  });
+  
